31154103eab81e14d4b21b4cd0e4232301d2d489
SeungbeomHu <hsb0423@naver.com>
2022-10-23 10:22:20 +0900

    make Header , Main src

---
+++ userin.c
@@ -0,0 +1,29 @@
+#include "smallsh.h"
+
+int userin(char* p){
+    int c,count;
+    ptr = inpbuf;
+    tok = tokbuf;
+
+    printf("%s" ,p);
+    count = 0;
+
+    while (1){
+
+        if ((c = getchar()) == EOF){
+            return EOF;
+        }
+        if (count < MAXBUF)
+            inpbuf[count++] = c;
+        if (c == '\n' && count < MAXBUF) {
+            inpbuf[count] = '\0';
+            return count;
+        }
+        if (c == '\n' || count >= MAXBUF) {
+        printf("smallsh: input line too long\n");
+        count = 0;
+        printf("%s", p);
+    }
+    }
+
+}
